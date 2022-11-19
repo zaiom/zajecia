73c67571020b9b55a6f8eb0b04645cab2fe5f907
zaiom <zaiom333@gmail.com>
2022-11-19 18:00:51 +0100

    ostatni commit i koncze to zadanie

--- main.cpp
+++ main.cpp
@@ -2,13 +2,10 @@
 
 #include <iostream>
 
-<<<<<<< HEAD
 //Program wyswieta napis "Witaj wiosno :D"
-=======
 /// @brief Główna funkcja programu, wyświetla fajny komunikat ...
 ///
 /// @return zawsze zwraca wartość 0.
->>>>>>> doc
 
 int main() {
     std::cout << "Witaj wiosno :D"
